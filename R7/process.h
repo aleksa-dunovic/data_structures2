d3b94a0bf040413b220c17f013e16b80d12a8e1d
Aleksa Dunovic <aleksa.dunovic@gmail.com>
2021-06-23 21:31:00 -0400

    initialise R7

---
+++ R7/process.h
@@ -0,0 +1,12 @@
+/*
+ * -------------------------
+ * Student Name: Aleksa Dunovic
+ * Student ID: 140271610
+ * Student email: duno1610@mylaurier.ca
+ * -------------------------
+ */
+
+#ifndef PROCESS_H_
+#define PROCESS_H_
+
+#endif /* PROCESS_H_ */
