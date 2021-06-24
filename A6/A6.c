3984fa7dbcf710b7f5120d1c064a92b0c9ca718e
Aleksa Dunovic <aleksa.dunovic@gmail.com>
2021-06-24 16:05:27 -0400

    initialise A6

---
+++ A6/A6.c
@@ -0,0 +1,36 @@
+/*
+ * ---------------------------------
+ * Student Name:
+ * Student ID:
+ * Student Email:
+ * ---------------------------------
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include "A6.h"
+
+void remove_shortest(Process *processes, const int size) {
+	//your code here
+	return;
+}
+
+void reverse_stack(Stack *s) {
+	//your code here
+	return;
+}
+
+int contains_stack(Stack *s, Data *value) {
+	//your code here
+	return 0;
+}
+
+int resize_stack(Stack *s, int new_capacity) {
+	//your code here
+	return 0;
+}
+Stack* concat_stack(Stack *s1, Stack *s2) {
+	//your code here
+	return NULL;
+}
